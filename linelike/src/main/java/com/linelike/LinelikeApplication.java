0b1cbbb0ae6e061480ace831e426486576ce5923
pronounceA <zyank.wpo.zyank.wpo@gmail.com>
2022-09-14 11:58:06 +0900

    create gradle project

---
+++ linelike/src/main/java/com/linelike/LinelikeApplication.java
@@ -0,0 +1,13 @@
+package com.linelike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinelikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinelikeApplication.class, args);
+	}
+
+}
---
+++ linelike/src/test/java/com/linelike/LinelikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linelike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinelikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
